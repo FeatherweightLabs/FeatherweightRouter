50a49df48893fd0e8e68617c64e9172824046e77
Karl Bowden <karl@karlbowden.com>
2016-01-07 23:39:49 +1100

    Create empty JunctionViewController

---
+++ Sources/Core/JunctionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  JunctionViewController.swift
+//  Beeline
+//
+//  Created by Karl Bowden on 7/01/2016.
+//  Copyright © 2016 Featherweight Labs. All rights reserved.
+//
+
+import UIKit
+
+public struct JunctionViewController {
+
+}
