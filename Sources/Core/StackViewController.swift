d628ac1adae86089c7f520fa7ef830fa3770ff60
Karl Bowden <karl@karlbowden.com>
2016-01-07 17:55:56 +1100

    Fix file name {Segment,Stack}ViewController

--- Sources/Core/StackViewController.swift
+++ Sources/Core/StackViewController.swift
@@ -1,10 +1,12 @@
 //
-//  SegmentViewController.swift
+//  StackViewController.swift
 //  Beeline
 //
 //  Created by Karl Bowden on 5/01/2016.
 //  Copyright © 2016 Featherweight Labs. All rights reserved.
 //
+
+import UIKit
 
 public class StackViewController: UINavigationController {
 
